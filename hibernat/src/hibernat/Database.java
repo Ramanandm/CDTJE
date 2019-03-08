aadd4419f2e86315441b88d00ffdc3bb5a8a5093
Ramanandm <48120731+Ramanandm@users.noreply.github.com>
2019-03-08 10:47:14 +0530

    hibernat demo

---
+++ hibernat/src/hibernat/Database.java
@@ -0,0 +1,26 @@
+package hibernat;
+
+public class Database
+{
+	int itemid;
+	String itemname;
+	int price;
+	public int getItemid() {
+		return itemid;
+	}
+	public void setItemid(int itemid) {
+		this.itemid = itemid;
+	}
+	public String getItemname() {
+		return itemname;
+	}
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}	
+}
---
+++ hibernat/src/hibernat/hibernat.java
@@ -0,0 +1,38 @@
+package hibernat;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class hibernat 
+{
+	public static void main(String arg[])
+	{
+		
+		Configuration config=new Configuration();
+		config.configure("hibernate.cfg.xml");
+		
+		SessionFactory sessionFactory=config.buildSessionFactory();
+		Session session=sessionFactory.openSession();
+		Transaction transaction=session.beginTransaction();
+		
+		com.Product product=new com.Product();
+		
+		product.setProductId(1001);
+		product.setProductName("Samsung J7");
+		product.setPrice(7000);
+		
+		session.save(product);
+		
+		transaction.commit();
+		
+		session.close();
+		
+		System.out.println("Object Saved in Database");
+		
+	}
+}
+
+	
+
